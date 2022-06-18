18a94c6212d8b566f4a2ecc86f896bf5403ac5bc
davewatters <89341297+davewatters@users.noreply.github.com>
2022-06-18 10:15:25 +0100

    add products & categories to admin panel: resolve #23

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Prod_category, Product
 
-# Register your models here.
+
+@admin.register(Prod_category)
+class Prod_categoryAdmin(admin.ModelAdmin):
+    '''
+    Define the Product Category fields shown in the admin panel.
+    '''
+    list_display = ('desc', 'disp_name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''
+    Define the Product fields shown in the admin panel.
+    '''
+    list_display = (
+        'code',
+        'desc',
+        'category',
+        'recurring_bill',
+        'sell_price',
+    )

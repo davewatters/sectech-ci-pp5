8852ed028087936a99bd20120bf6e6709b313aec
davewatters <89341297+davewatters@users.noreply.github.com>
2022-07-15 18:42:21 +0100

    refactor: cust logins to block unauthorised manual access

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 
 from .forms import CustomerForm
 from .models import Customer, Customer_product
@@ -11,6 +11,11 @@ from .models import Customer, Customer_product
 def customer_create(request):
     '''View shows new customer form'''
     customer = get_object_or_404(Customer, user=request.user.id)
+
+    # stop user access by typing url
+    if customer.name:
+        messages.error(request, 'Customer already setup.')
+        return redirect('customer-detail', customer.id)
 
     if request.method == 'POST':
         form = CustomerForm(request.POST, instance=customer)
@@ -38,7 +43,14 @@ def customer_detail(request, customer_id):
     Allows user to update details.
     Includes views of customer's products and past invoices.
     '''
+
     customer = get_object_or_404(Customer, id=customer_id)
+    # Customer record can only be opened by correct user
+    if request.user.id != customer.user_id:
+        messages.error(request, 'Unauthorized to view that customer page.')
+        return redirect(settings.LOGIN_REDIRECT_URL)
+        # return HttpResponse(status=403)
+
     cust_products = Customer_product.objects.filter(customer=customer_id)
 
     if request.method == 'POST':

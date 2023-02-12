03f98b3a983e1c94c5528c479e843209ac27322a
davewatters <89341297+davewatters@users.noreply.github.com>
2023-02-12 18:57:47 +0000

    fix: add robots & sitemap to urls.py, view as template

--- sectech/urls.py
+++ sectech/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -26,5 +27,9 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('customers/', include('customers.urls')),
     path('products/', include('products.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt",
+                                            content_type="text/plain"),),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml",
+                                             content_type="text/xml"),),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
